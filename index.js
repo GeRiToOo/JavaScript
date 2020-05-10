c78d42a9a9525530444d31e51b711656d23dacf5
Gergana Rangelova <57684252+GeRiToOo@users.noreply.github.com>
2020-05-10 16:38:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const target = 15;
+const userValue = "15";
+
+const password = "JSisKool888";
+const passwordConfirmation = "JSisCool888";
+
+const sortOrder = "ALPHABETICAL";
+
+// ADD YOUR CODE BELOW
+
+let  targetMatchesCoercedUserValue = true;
+
+let targetMatchesUserValue = false;
+
+let passwordMatchesConfirmation = false;
+
+let passwordMessage = "Oops, your password and confirmation do not match!";
+
+var fruits = "banana, cherry, watermelon";
