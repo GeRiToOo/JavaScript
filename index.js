7cc3603179cce24732e02a402ebe12327ffec014
Gergana Rangelova <57684252+GeRiToOo@users.noreply.github.com>
2020-05-10 16:43:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //constant denotes that a variable is a constant
 
 const target = 15;
+//number
 const userValue = "15";
+//string
 
 const password = "JSisKool888";
 const passwordConfirmation = "JSisCool888";
@@ -11,11 +13,16 @@ const sortOrder = "ALPHABETICAL";
 // let allows you to declare variables that are limited to the scope of a block statement
 
 let  targetMatchesCoercedUserValue = true;
+//boolean 
 
 let targetMatchesUserValue = false;
+//boolean 
 
 let passwordMatchesConfirmation = false;
+//boolean 
 
 let passwordMessage = "Oops, your password and confirmation do not match!";
+//string
 
 var fruits = "banana, cherry, watermelon";
+//string
