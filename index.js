8ab4cdd2c0aea7735ccd79081af182759ff780e3
Gergana Rangelova <57684252+GeRiToOo@users.noreply.github.com>
2020-05-10 16:39:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//constant denotes that a variable is a constant
+
 const target = 15;
 const userValue = "15";
 
@@ -6,7 +8,7 @@ const passwordConfirmation = "JSisCool888";
 
 const sortOrder = "ALPHABETICAL";
 
-// ADD YOUR CODE BELOW
+// let allows you to declare variables that are limited to the scope of a block statement
 
 let  targetMatchesCoercedUserValue = true;
 
